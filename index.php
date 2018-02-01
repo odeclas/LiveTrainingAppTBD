907056ad24e59084fba3c635d951c465bced952e
Ivan Salcedo <dev@ivansalcedo.com>
2018-02-01 20:21:43 +0000

    edits
    Added new text

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 echo "<h1>Check that out!</h1>";
 echo "<h2>Quixis did it</h2>";
+echo "<h4>Quixis did it again</h4>";
 echo "<p>You've just created your first Web App for Containers app!</p>";
 echo "<p>You've just created your very own hello world!</p>";
-echo "<p><a href=“index2.php”>Link to other page</a></p>";
+echo "<p><a href=index2.php>Link to other page</a></p>";
 ?>
