98bac7e56890fc9a2a3fbd3562a42c424d768702
Ivan Salcedo <dev@ivansalcedo.com>
2018-02-01 20:48:12 +0000

    add index3
    A description that is unnecessary.

---
+++ index3.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Second page</h1>";
+echo "<h2>Quixis did it</h2>";
+echo "<p>You've just created your first Web App for Containers app!</p>";
+echo "<p>You've just created your very own hello world!</p>";
+?>
